1c3e0db45e30db98916d02689acea6bde3759af8
nagavallim <nagavalli.m@skava.com>
2019-07-09 23:29:28 +0530

    UI done : React hooks included for state handling

--- src/bookMyPizza.js
+++ src/bookMyPizza.js
@@ -1,53 +1,69 @@
-import React from 'react'
-import { pizzaStore } from './pizzaStore'
+import React, { useState } from 'react'
 // import './styles.scss'
 
-function Incrementor() {
-    return <button className="incrementor" onClick={() => console.log('increment')} children="+" />
+function Incrementor(props) {
+    return <button className="incrementor" onClick={props.onIncrement} children="+" />
 }
-function Decrementor() {
-    return <button className="decrementor" onClick={() => console.log('decrement')} children="-" />
+function Decrementor(props) {
+    return <button className="decrementor" onClick={props.onDecrement} children="-" />
 }
 
-class BookMyPizza extends React.Component {
-    static defaultProps = {
-        pizzaVarietyWithPrice: {
+function pizzaOrderView(props) {
+    const { store, list } = props
+    let pizzaView = []
+    for(let item in list) {
+        const count = store[item+'Count']
+        const countHandler = item+'CountHandler'
+        pizzaView.push(
+            <li>
+                {item}:
+                <Decrementor onDecrement={() => count > 0 && store[countHandler](--count) } />
+                {count}
+                <Incrementor onIncrement={() => count < 10 && store[countHandler](++count)} />
+            </li>)
+    }
+    return pizzaView
+}
+
+function pizzaStore() {
+    const [smallCount, smallCountHandler] = useState(0)
+    const [mediumCount, mediumCountHandler] = useState(0)
+    const [largeCount, largeCountHandler] = useState(0)
+    const [childrenCount, childrenCountHandler] = useState(0)
+    const [adultCount, adultCountHandler] = useState(0)
+
+    return {
+        smallCount, smallCountHandler,
+        mediumCount, mediumCountHandler,
+        largeCount, largeCountHandler,
+        childrenCount, childrenCountHandler,
+        adultCount, adultCountHandler
+    }
+}
+
+function BookMyPizza(props) {
+    const {
+        pizzaVarietyWithPrice = {
             small: 100,
             medium: 200,
             large: 300,
         },
-        maxQuantityPerPerson: {
+        maxQuantityPerPerson = {
             children: 100,
             adult: 200,
         },
-        minimumPay: 200,
-    }
-    pizzaHandler() {
-        let pizzaView = []
-        for(let item in this.props.pizzaVarietyWithPrice) {
-            pizzaView.push(
-                <li>
-                    {item}:
-                    <Decrementor />
-                    <Incrementor />
-                </li>)
-        }
-        return pizzaView
-    }
-    /* <div style={{display: 'flex'}}>
-        {item}:
-        <button children="-" />
-        0
-        <button children="+" />
-    </div> */
-    render() {
-        return (
-            <div className="wrapper">
-                <h2 style={{textAlign: 'center'}}>Book your yummy PIZZA...</h2>
-                {this.pizzaHandler()}
-            </div>
-        )
-    }
+        minimumPay = 200,
+    } = props
+
+    const store = pizzaStore()
+    
+    return (
+        <div className="wrapper">
+            <h2 style={{textAlign: 'center'}}>Book your yummy PIZZA...</h2>
+            {pizzaOrderView({list: pizzaVarietyWithPrice, minimumPay, store})}
+            {pizzaOrderView({list: maxQuantityPerPerson, minimumPay, store})}
+        </div>
+    )
 }
 
 export default BookMyPizza
